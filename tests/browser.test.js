7c09ec12e036cf9cb4db0d8a362914f7b69ef031
Filip Nordqvist <114819894+FilipNordqvist@users.noreply.github.com>
2025-05-05 20:44:48 +0200

    Kollar igenom alla steg igen!!

--- tests/browser.test.js
+++ tests/browser.test.js
@@ -2,10 +2,9 @@ const { Builder, By, until } = require('selenium-webdriver');
 require('geckodriver');
 
 const fileUnderTest = 'file://' + __dirname.replace(/ /g, '%20') + '/../dist/index.html';
-
 const defaultTimeout = 10000;
 let driver;
-jest.setTimeout(1000 * 60 * 1); // 5 minuter
+jest.setTimeout(1000 * 60 * 5); // 5 minuter
 
 // Det här körs innan vi kör testerna för att säkerställa att Firefox är igång
 beforeAll(async () => {
@@ -14,7 +13,10 @@ console.log(fileUnderTest);
     await driver.get(fileUnderTest);
 });
 
-
+// Allra sist avslutar vi Firefox igen
+afterAll(async() => {
+    await driver.quit();
+}, defaultTimeout);
 
 test('The stack should be empty in the beginning', async () => {
     let stack = await driver.findElement(By.id('top_of_stack')).getText();
@@ -30,8 +32,3 @@ describe('Clicking "Pusha till stacken"', () => {
         await alert.accept();
     });
 });
-
-// Allra sist avslutar vi Firefox igen
-afterAll(async() => {
-    await driver.quit();
-}, defaultTimeout);
