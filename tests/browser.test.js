22215105b5d5273969e2d74eeecf2ca1a429dd4e
Filip Nordqvist <114819894+FilipNordqvist@users.noreply.github.com>
2025-05-05 20:41:41 +0200

    Försöker hitta felet

--- tests/browser.test.js
+++ tests/browser.test.js
@@ -2,9 +2,10 @@ const { Builder, By, until } = require('selenium-webdriver');
 require('geckodriver');
 
 const fileUnderTest = 'file://' + __dirname.replace(/ /g, '%20') + '/../dist/index.html';
+
 const defaultTimeout = 10000;
 let driver;
-jest.setTimeout(1000 * 60 * 5); // 5 minuter
+jest.setTimeout(1000 * 60 * 1); // 5 minuter
 
 // Det här körs innan vi kör testerna för att säkerställa att Firefox är igång
 beforeAll(async () => {
@@ -13,10 +14,7 @@ console.log(fileUnderTest);
     await driver.get(fileUnderTest);
 });
 
-// Allra sist avslutar vi Firefox igen
-afterAll(async() => {
-    await driver.quit();
-}, defaultTimeout);
+
 
 test('The stack should be empty in the beginning', async () => {
     let stack = await driver.findElement(By.id('top_of_stack')).getText();
@@ -32,3 +30,8 @@ describe('Clicking "Pusha till stacken"', () => {
         await alert.accept();
     });
 });
+
+// Allra sist avslutar vi Firefox igen
+afterAll(async() => {
+    await driver.quit();
+}, defaultTimeout);
--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,8 @@ const path = require('path');
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-    mode: 'production',
     entry: './src/index.js',
+    mode: 'production',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.js'
