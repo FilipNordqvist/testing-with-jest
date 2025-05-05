ab5b6d6e6563b11e4d36c7a707e6c4a5f35cc014
Filip Nordqvist <114819894+FilipNordqvist@users.noreply.github.com>
2025-05-05 19:58:36 +0200

    La till ytliggare en index för test

--- tests/stack.test.js
+++ tests/stack.test.js
@@ -17,3 +17,17 @@ test('peek on stack with two or more elements returns the top element', () => {
     expect(stack.peek()).toBeDefined();
     expect(stack.peek()).toBe(42);
 });
+
+//Filips gör ett felaktig test som inte går igenom då den ger 1 då den behöver popas en gång till.
+test('pop on empty stack returns undefined', () => {
+    stack.pop();
+    stack.pop();
+    stack.pop();
+    expect(stack.pop()).toBeUndefined();
+});
+
+//Filip testet push?
+test('pusch 1 on empty stack returns 1?', () => {
+    stack.push(1)
+    expect(stack.pop()).toBe(1);
+});
